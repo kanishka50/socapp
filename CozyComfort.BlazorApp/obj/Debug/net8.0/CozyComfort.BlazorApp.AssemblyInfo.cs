557c0b52b0e276325ab53652e46afa49537c4a8f
Kanishka50 <dilhanpremarathna2001@gmail.com>
2025-06-27 14:01:04 +0530

    ui updates 3

--- CozyComfort.BlazorApp/obj/Debug/net8.0/CozyComfort.BlazorApp.AssemblyInfo.cs
+++ CozyComfort.BlazorApp/obj/Debug/net8.0/CozyComfort.BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CozyComfort.BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476e85044cf4a9f8ae7e0bafff4570b1600f48be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a039c9807841d00eca3860b2a98f1c7759cafc58")]
 [assembly: System.Reflection.AssemblyProductAttribute("CozyComfort.BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CozyComfort.BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
